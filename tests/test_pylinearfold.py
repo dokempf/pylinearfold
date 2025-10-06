7dc17933bb726a73022dc9645ca52027ca3b00d4
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2025-10-06 15:07:26 +0200

    Add pylinearpartition smoke test

--- tests/test_pylinearfold.py
+++ tests/test_pylinearfold.py
@@ -5,3 +5,8 @@ def test_pylinearfold():
     res = pylinearfold.fold("GGGGAAAACCCC")
     assert res.structure == "((((....))))"
     assert res.score == -5.4
+
+
+def test_pylinearpartition():
+    res = pylinearfold.fold("GGGGAAAACCCC")
+    assert res < 1.0
