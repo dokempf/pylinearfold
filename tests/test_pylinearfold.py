f3923ec27b8ec54e73f8488cb8624246b3d62af1
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2025-10-20 10:54:12 +0200

    Make probability return type numpy structured array

--- tests/test_pylinearfold.py
+++ tests/test_pylinearfold.py
@@ -11,3 +11,18 @@ def test_pylinearpartition():
     res = pylinearfold.partition("GGGGAAAACCCC")
     assert res["structure"] == "((((....))))"
     assert -5.6 < res["free_energy"] < -5.5
+
+    # Probability matrix should only show high probabilities
+    # for 1-12, 2-11, 3-10, 4-9 pairs
+    for i in range(1, 13):
+        for j in range(1, 13):
+            if i >= j:
+                continue
+            mask = (res["probabilities"]["i"] == i) & (res["probabilities"]["j"] == j)
+            filtered = res["probabilities"][mask]
+
+            if i + j == 13 and i < 5:
+                assert filtered.size == 1
+                assert filtered["prob"][0] > 0.8
+            else:
+                assert filtered.size == 0 or filtered["prob"][0] < 0.2
